f4afd206ab667591bc5623b3eaf01efa1cd809f5
Marian Plösch <marian@bitflut.com>
2020-05-10 21:50:09 +0200

    test: e2e tests and travis

--- apps/ng-integration-e2e/src/integration/app.spec.ts
+++ apps/ng-integration-e2e/src/integration/app.spec.ts
@@ -1,13 +1,48 @@
-import { getGreeting } from '../support/app.po';
-
 describe('ng-integration', () => {
-  beforeEach(() => cy.visit('/'));
+    beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    it('should display posts', () => {
+        cy.get('a').contains('Posts').click();
+        cy.contains('#100').should('exist');
+        cy.contains('#1010').should('not.exist');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to ng-integration!');
-  });
+        cy.contains('#100').click();
+        cy.get('app-posts-show').contains('#100').should('exist');
+    });
+
+    it('should display paginated posts', () => {
+        cy.get('a').contains('Paginated').click();
+        cy.contains('#100').should('not.exist');
+        cy.contains('#5').should('exist');
+        cy.contains('#10').should('not.exist');
+
+        cy.contains('#5').click();
+        cy.get('app-posts-show').contains('#5').should('exist');
+
+        cy.contains('Load more').should('exist');
+        cy.contains('Load more').click();
+        cy.contains('#5').should('exist');
+        cy.contains('#10').should('exist');
+    });
+
+    it('should populate & display comments', () => {
+        cy.get('a').contains('With comments').click();
+        cy.contains('#10').should('not.exist');
+        cy.contains('#5').should('exist');
+        cy.contains('#5').click();
+
+        cy.get('app-posts-show').get('lyxs-many').contains('Noemie@marques.me').should('exist');
+    });
+
+    it('should populate & display users & comments', () => {
+        cy.get('a').contains('With user & comments').click();
+        cy.contains('#10').should('not.exist');
+        cy.contains('#4').should('exist');
+        cy.get('lyxs-many').contains('Leanne Graham').should('exist');
+        cy.contains('#4').click();
+        cy.wait(2000);
+
+        cy.get('app-posts-show').contains('Sincere@april.biz').should('exist');
+        cy.get('app-posts-show').get('lyxs-many').contains('Christine@ayana.info').should('exist');
+    });
 });
--- apps/ng-integration/src/app/posts/posts-index/posts-index.component.ts
+++ apps/ng-integration/src/app/posts/posts-index/posts-index.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-posts-index',
     templateUrl: './posts-index.component.html',
-    styleUrls: ['./posts-index.component.css']
+    styleUrls: ['./posts-index.component.css'],
 })
 export class PostsIndexComponent implements OnInit {
 
