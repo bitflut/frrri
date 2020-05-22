28ecf3258c4f0ddbd7bbf3c5cfab49672a9cdb95
Patrick Thonhauser <patrick.thonhauser@gmail.com>
2020-05-22 11:31:25 +0200

    refactor: rename router to router-middleware

--- libs/router-middleware/jest.config.js
+++ libs/router-middleware/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'routes',
+  name: 'router-middleware',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/routes',
+  coverageDirectory: '../../coverage/libs/router-middleware',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
