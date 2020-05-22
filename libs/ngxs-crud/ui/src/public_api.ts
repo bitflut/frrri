9840740b0baa8c9946bf43be6e8b140221f37820
Patrick Thonhauser <patrick.thonhauser@gmail.com>
2020-05-22 11:25:41 +0200

    fix: imports

--- libs/ngxs-crud/ui/src/public_api.ts
+++ libs/ngxs-crud/ui/src/public_api.ts
@@ -1,7 +1,7 @@
 export { ActiveComponent } from './components/active-component/active.component';
-export { NgxsCrudActiveModule } from './components/active-component/active.Module';
+export { NgxsCrudActiveModule } from './components/active-component/active.module';
 export { ManyComponent } from './components/many-component/many.component';
-export { NgxsCrudManyModule } from './components/many-component/many.Module';
+export { NgxsCrudManyModule } from './components/many-component/many.module';
 export { OneComponent } from './components/one-component/one.component';
-export { NgxsCrudOneModule } from './components/one-component/one.Module';
+export { NgxsCrudOneModule } from './components/one-component/one.module';
 
